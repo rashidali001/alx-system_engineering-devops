3eaef7d1be0c11ddf3fe7a8d19a158040d284f20
rashidali001 <rashidestonian@gmail.com>
2023-01-11 14:24:42 -0800

    CM

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # using Puppet to create file /tmp
 
-file { '/tmp':
-  path  => '/tmp/school',
-  mode  => '0744',
-  owner => 'www-data',
-  group => 'www-data',
+file { '/tmp/school':
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
 
