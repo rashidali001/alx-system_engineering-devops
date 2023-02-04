6c3c4dd75595cbc4fefb2101ec212f7b4770d01a
rashidali001 <rashidestonian@gmail.com>
2023-02-04 15:48:21 -0800

    scp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,5 @@
+#  configuring your server with Puppet!
+exec {'install':
+      provider => shell,
+      command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com\/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+}
