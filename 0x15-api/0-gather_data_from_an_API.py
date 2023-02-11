6ec80ac8bd64192f80a51abc4a71aa12aa9f036c
rashidali001 <rashidestonian@gmail.com>
2023-02-11 12:44:33 +0300

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,45 @@
+'''
+0-gather_data_from_an_API - gets data from an API
+'''
 #!/usr/bin/python3
-from urllib import request
 import json
+from urllib import request
 import sys
 
-argument = int(sys.argv[1])
 
-resp = request.urlopen('https://jsonplaceholder.typicode.com/users')
-resp2 = request.urlopen('https://jsonplaceholder.typicode.com/todos')
+if __name__ == "__main__":
+    argument = int(sys.argv[1])
+    resp = request.urlopen('https://jsonplaceholder.typicode.com/users')
+    resp2 = request.urlopen('https://jsonplaceholder.typicode.com/todos')
+    html = resp.read().decode("UTF-8")
+    html2 = resp2.read().decode("UTF-8")
 
-html = resp.read().decode("UTF-8")
-html2 = resp2.read().decode("UTF-8")
+    # converts the JSON file to an object literal
+    data = json.loads(html)
+    userData = json.loads(html2)
 
-# converts the JSON file to an object literal
-data = json.loads(html)
-userData = json.loads(html2)
+    user = ''
+    totalTasks = 0
+    finishedTasks = 0
 
-user = ''
-totalTasks = 0
-finishedTasks = 0
+    for obj in data:
+        if obj['id'] == argument:
+            user = obj
 
-for obj in data:
-    if obj['id'] == argument:
-        user = obj
+    for obj in userData:
+        if obj['userId'] == argument:
+            if obj['completed'] == True:
+                finishedTasks += 1
+            totalTasks += 1
 
-for obj in userData:
-    if obj['userId'] == argument:
-        if obj['completed'] == True:
-            finishedTasks += 1
-        totalTasks += 1
+    userName = user['name']
 
-userName = user['name']
+    print(f"Employee {userName} "
+          f"is done with tasks ({finishedTasks}/{totalTasks}):")
 
-print(f"Employee {userName} "
-      f"is done with tasks ({finishedTasks}/{totalTasks}):")
-
-for obj in userData:
-    if obj['userId'] == argument:
-        if obj['completed'] == True:
-            print("\t ", end="")
-            print(obj['title'])
-
-
+    for obj in userData:
+        if obj['userId'] == argument:
+            if obj['completed'] == True:
+                print("\t ", end="")
+                print(obj['title'])
+                
